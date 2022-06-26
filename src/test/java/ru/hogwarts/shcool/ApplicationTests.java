93508e34f09b844376b518ec836aaa05a06e951c
leesr1972 <sergey.r.lee@gmail.com>
2022-06-26 20:58:43 +0300

    Домашнее задание к уроку 3.5 выполнено.

--- src/test/java/ru/hogwarts/shcool/ApplicationTests.java
+++ src/test/java/ru/hogwarts/shcool/ApplicationTests.java
@@ -1,13 +1,59 @@
 package ru.hogwarts.shcool;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import ru.hogwarts.shcool.controller.StudentController;
+import ru.hogwarts.shcool.model.Student;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private StudentController studentController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    public void contextLoads() throws Exception {
+        Assertions.assertThat(studentController).isNotNull();
     }
 
+    @Test
+    public void testPostStudent() throws Exception {
+        Student student = new Student();
+        student.setId(7L);
+        student.setName("Bob");
+        student.setAge(26);
+
+        Assertions
+                .assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/student", student, String.class))
+                .isNotNull();
+    }
+
+    @Test
+    public void testGetStudent() throws Exception {
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/student/1", String.class))
+                .isNotNull();
+    }
+
+    @Test
+    public void testPutStudent() throws Exception {
+        Student student = new Student();
+        student.setId(2L);
+        student.setName("Михаил");
+        student.setAge(26);
+
+        Assertions
+                .assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/student", student, String.class))
+                .isNotNull();
+    }
 }
